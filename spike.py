0413047ec9592c7fd393ce3113b8245efa699fd9
shmezi <alexirving992@gmail.com>
2022-06-27 16:42:58 +0300

    Initial commit

---
+++ spike.py
@@ -0,0 +1,668 @@
+from enum import Enum
+
+
+class Sound(Enum):
+    Alert = "Alert"
+    Applause_1 = "Applause 1"
+    Applause_2 = "Applause 2"
+    Applause_3 = "Applause 3"
+    Baa = "Baa"
+    Bang_1 = "Bang 1"
+    Bang_2 = "Bang 2"
+    Basketball_Bounce = "Basketball Bounce"
+    Big_Boing = "Big Boing"
+    Bird = "Bird"
+    Bite = "Bite"
+    Boat_Horn_1 = "Boat Horn 1"
+    Boat_Horn_2 = "Boat Horn 2"
+    Bonk = "Bonk"
+    Boom_Cloud = "Boom Cloud"
+    Boop_Bing_Bop = "Boop Bing Bop"
+    Bowling_Strike = "Bowling Strike"
+    Burp_1 = "Burp 1"
+    Burp_2 = "Burp 2"
+    Burp_3 = "Burp 3"
+    Car_Accelerate_1 = "Car Accelerate 1"
+    Car_Accelerating_2 = "Car Accelerating 2"
+    Car_Horn = "Car Horn"
+    Car_Idle = "Car Idle"
+    Car_Reverse = "Car Reverse"
+    Car_Skid_1 = "Car Skid 1"
+    Car_Skid_2 = "Car Skid 2"
+    Car_Vroom = "Car Vroom"
+    Cat_Angry = "Cat Angry"
+    Cat_Happy = "Cat Happy"
+    Cat_Hiss = "Cat Hiss"
+    Cat_Meow_1 = "Cat Meow 1"
+    Cat_Meow_2 = "Cat Meow 2"
+    Cat_Meow_3 = "Cat Meow 3"
+    Cat_Purring = "Cat Purring"
+    Cat_Whining = "Cat Whining"
+    Chatter = "Chatter"
+    Chirp = "Chirp"
+    Clang = "Clang"
+    Clock_Ticking = "Clock Ticking"
+    Clown_Honk_1 = "Clown Honk 1"
+    Clown_Honk_2 = "Clown Honk 2"
+    Clown_Honk_3 = "Clown Honk 3"
+    Coin = "Coin"
+    Collect = "Collect"
+    Connect = "Connect"
+    Crank = "Crank"
+    Crazy_Laugh = "Crazy Laugh"
+    Croak = "Croak"
+    Crowd_Gasp = "Crowd Gasp"
+    Crunch = "Crunch"
+    Cuckoo = "Cuckoo"
+    Cymbal_Crash = "Cymbal Crash"
+    Disconnect = "Disconnect"
+    Dog_Bark_1 = "Dog Bark 1"
+    Dog_Bark_2 = "Dog Bark 2"
+    Dog_Bark_3 = "Dog Bark 3"
+    Dog_Whining_1 = "Dog Whining 1"
+    Dog_Whining_2 = "Dog Whining 2"
+    Doorbell_1 = "Doorbell 1"
+    Doorbell_2 = "Doorbell 2"
+    Doorbell_3 = "Doorbell 3"
+    Door_Closing = "Door Closing"
+    Door_Creek_1 = "Door Creek 1"
+    Door_Creek_2 = "Door Creek 2"
+    Door_Handle = "Door Handle"
+    Door_Knock = "Door Knock"
+    Door_Slam_1 = "Door Slam 1"
+    Door_Slam_2 = "Door Slam 2"
+    Drum_Roll = "Drum Roll"
+    Dun_Dun_Dunnn = "Dun Dun Dunnn"
+    Emotional_Piano = "Emotional Piano"
+    Fart_1 = "Fart 1"
+    Fart_2 = "Fart 2"
+    Fart_3 = "Fart 3"
+    Footsteps = "Footsteps"
+    Gallop = "Gallop"
+    Glass_Breaking = "Glass Breaking"
+    Glug = "Glug"
+    Goal_Cheer = "Goal Cheer"
+    Gong = "Gong"
+    Growl = "Growl"
+    Grunt = "Grunt"
+    Hammer_Hit = "Hammer Hit"
+    Head_Shake = "Head Shake"
+    High_Whoosh = "High Whoosh"
+    Jump = "Jump"
+    Jungle_Frogs = "Jungle Frogs"
+    Laser_1 = "Laser 1"
+    Laser_2 = "Laser 2"
+    Laser_3 = "Laser 3"
+    Laughing_Baby_1 = "Laughing Baby 1"
+    Laughing_Baby_2 = "Laughing Baby 2"
+    Laughing_Boy = "Laughing Boy"
+    Laughing_Crowd_1 = "Laughing Crowd 1"
+    Laughing_Crowd_2 = "Laughing Crowd 2"
+    Laughing_Girl = "Laughing Girl"
+    Laughing_Male = "Laughing Male"
+    Lose = "Lose"
+    Low_Boing = "Low Boing"
+    Low_Squeak = "Low Squeak"
+    Low_Whoosh = "Low Whoosh"
+    Magic_Spell = "Magic Spell"
+    Male_Jump_1 = "Male Jump 1"
+    Male_Jump_2 = "Male Jump 2"
+    Moo = "Moo"
+    Ocean_Wave = "Ocean Wave"
+    Oops = "Oops"
+    Orchestra_Tuning = "Orchestra Tuning"
+    Party_Blower = "Party Blower"
+    Pew = "Pew"
+    Ping_Pong_Hit = "Ping Pong Hit"
+    Plane_Flying_By = "Plane Flying By"
+    Plane_Motor_Running = "Plane Motor Running"
+    Plane_Starting = "Plane Starting"
+    Pluck = "Pluck"
+    Police_Siren_1 = "Police Siren 1"
+    Police_Siren_2 = "Police Siren 2"
+    Police_Siren_3 = "Police Siren 3"
+    Punch = "Punch"
+    Rain = "Rain"
+    Ricochet = "Ricochet"
+    Rimshot = "Rimshot"
+    Ring_Tone = "Ring Tone"
+    Rip = "Rip"
+    Robot_1 = "Robot 1"
+    Robot_2 = "Robot 2"
+    Robot_3 = "Robot 3"
+    Rocket_Explosion_Rumble = "Rocket Explosion Rumble"
+    Rooster = "Rooster"
+    Scrambling_Feet = "Scrambling Feet"
+    Screech = "Screech"
+    Seagulls = "Seagulls"
+    Service_Announcement = "Service Announcement"
+    Sewing_Machine = "Sewing Machine"
+    Ship_Bell = "Ship Bell"
+    Siren_Whistle = "Siren Whistle"
+    Skid = "Skid"
+    Slide_Whistle_1 = "Slide Whistle 1"
+    Slide_Whistle_2 = "Slide Whistle 2"
+    Sneaker_Squeak = "Sneaker Squeak"
+    Snoring = "Snoring"
+    Snort = "Snort"
+    Space_Ambience = "Space Ambience"
+    Space_Flyby = "Space Flyby"
+    Space_Noise = "Space Noise"
+    Splash = "Splash"
+    Sport_Whistle_1 = "Sport Whistle 1"
+    Sport_Whistle_2 = "Sport Whistle 2"
+    Squeaky_Toy = "Squeaky Toy"
+    Squish_Pop = "Squish Pop"
+    Suction_Cup = "Suction Cup"
+    Tada = "Tada"
+    Telephone_Ring_2 = "Telephone Ring 2"
+    Telephone_Ring = "Telephone Ring"
+    Teleport_2 = "Teleport 2"
+    Teleport_3 = "Teleport 3"
+    Teleport = "Teleport"
+    Tennis_Hit = "Tennis Hit"
+    Thunder_Storm = "Thunder Storm"
+    Toliet_Flush = "Toliet Flush"
+    Toy_Honk = "Toy Honk"
+    Toy_Zing = "Toy Zing"
+    Traffic = "Traffic"
+    Train_Breaks = "Train Breaks"
+    Train_Horn_1 = "Train Horn 1"
+    Train_Horn_2 = "Train Horn 2"
+    Train_Horn_3 = "Train Horn 3"
+    Train_On_Tracks = "Train On Tracks"
+    Train_Signal_1 = "Train Signal 1"
+    Train_Signal_2 = "Train Signal 2"
+    Train_Start = "Train Start"
+    Train_Whistle = "Train Whistle"
+    Triumph = "Triumph"
+    Tropical_Birds = "Tropical Birds"
+    Wand = "Wand"
+    Water_Drop = "Water Drop"
+    Whistle_Thump = "Whistle Thump"
+    Whiz_1 = "Whiz 1"
+    Whiz_2 = "Whiz 2"
+    Window_Breaks = "Window Breaks"
+    Win = "Win"
+    Wobble = "Wobble"
+    Wood_Tap = "Wood Tap"
+    Zip = "Zip"
+
+
+class Image(Enum):
+    ANGRY = "ANGRY"
+    ARROW_E = "ARROW_E"
+    ARROW_N = "ARROW_N"
+    ARROW_NE = "ARROW_NE"
+    ARROW_NW = "ARROW_NW"
+    ARROW_S = "ARROW_S"
+    ARROW_SE = "ARROW_SE"
+    ARROW_SW = "ARROW_SW"
+    ARROW_W = "ARROW_W"
+    ASLEEP = "ASLEEP"
+    BUTTERFLY = "BUTTERFLY"
+    CHESSBOARD = "CHESSBOARD"
+    CLOCK1 = "CLOCK1"
+    CLOCK10 = "CLOCK10"
+    CLOCK11 = "CLOCK11"
+    CLOCK12 = "CLOCK12"
+    CLOCK2 = "CLOCK2"
+    CLOCK3 = "CLOCK3"
+    CLOCK4 = "CLOCK4"
+    CLOCK5 = "CLOCK5"
+    CLOCK6 = "CLOCK6"
+    CLOCK7 = "CLOCK7"
+    CLOCK8 = "CLOCK8"
+    CLOCK9 = "CLOCK9"
+    CONFUSED = "CONFUSED"
+    COW = "COW"
+    DIAMOND = "DIAMOND"
+    DIAMOND_SMALL = "DIAMOND_SMALL"
+    DUCK = "DUCK"
+    FABULOUS = "FABULOUS"
+    GHOST = "GHOST"
+    GIRAFFE = "GIRAFFE"
+    GO_RIGHT = "GO_RIGHT"
+    GO_LEFT = "GO_LEFT"
+    GO_UP = "GO_UP"
+    GO_DOWN = "GO_DOWN"
+    HAPPY = "HAPPY"
+    HEART = "HEART"
+    HEART_SMALL = "HEART_SMALL"
+    HOUSE = "HOUSE"
+    MEH = "MEH"
+    MUSIC_CROTCHET = "MUSIC_CROTCHET"
+    MUSIC_QUAVER = "MUSIC_QUAVER"
+    MUSIC_QUAVERS = "MUSIC_QUAVERS"
+    NO = "NO"
+    PACMAN = "PACMAN"
+    PITCHFORK = "PITCHFORK"
+    RABBIT = "RABBIT"
+    ROLLERSKATE = "ROLLERSKATE"
+    SAD = "SAD"
+    SILLY = "SILLY"
+    SKULL = "SKULL"
+    SMILE = "SMILE"
+    SNAKE = "SNAKE"
+    SQUARE = "SQUARE"
+    SQUARE_SMALL = "SQUARE_SMALL"
+    STICKFIGURE = "STICKFIGURE"
+    SURPRISED = "SURPRISED"
+    SWORD = "SWORD"
+    TARGET = "TARGET"
+    TORTOISE = "TORTOISE"
+    TRIANGLE = "TRIANGLE"
+    TRIANGLE_LEFT = "TRIANGLE_LEFT"
+    TSHIRT = "TSHIRT"
+    UMBRELLA = "UMBRELLA"
+    XMAS = "XMAS"
+    YES = "YES"
+
+
+class Color(Enum):
+    AZURE = "azure"
+    BLACK = "black"
+    BLUE = "blue"
+    CYAN = "cyan"
+    GREEN = "green"
+    ORANGE = "orange"
+    PINK = "pink"
+    RED = "red"
+    VIOLET = "violet"
+    YELLOW = "yellow"
+    WHITE = "white"
+
+
+class Port(Enum):
+    A = 'A'
+    B = 'B'
+    C = 'C'
+    D = 'D'
+    E = 'E'
+    F = 'F'
+
+
+class Color(Enum):
+    BLACK = "black",
+    VIOLET = "violet"
+    BLUE = "blue"
+    CYAN = "cyan"
+    GREEN = "green"
+    YELLOW = "yellow"
+    RED = "red"
+    WHITE = "white"
+    NONE = None
+
+
+class Gesture(Enum):
+    SHAKEN = "shaken"
+    TAPPED = "tapped"
+    DOUBLE_TAPPED = "double-tapped"
+    FALLING = "falling"
+    NONE = "none"
+
+
+class StopAction(Enum):
+    BRAKE = "brake"
+    HOLD = "hold"
+    COAST = "coast"
+
+
+class Orientation(Enum):
+    FRONT = "front"
+    BACK = "back"
+    UP = "up"
+    DOWN = "down"
+    LEFTSIDE = "leftside"
+    RIGHTSIDE = "rightside"
+
+
+class DistanceUnit(Enum):
+    CM = "cm"
+    INCH = "in"
+
+
+class AdvancedDistanceUnit(DistanceUnit):
+    PERCENT = "%"
+
+
+class MotorUnit(AdvancedDistanceUnit):
+    ROTATIONS = "rotations"
+    DEGREES = "degrees"
+    SECONDS = "seconds"
+
+
+class App:
+    """
+    Represents the controller of the spike hub.
+    """
+
+    def play_sound(self, name: Sound, volume=100):
+        """
+        Plays a sound from the device (i.e., tablet or computer).
+        The program will not continue until the sound has finished playing.
+
+        Arguments:
+            name: Name of the sound to play
+            volume: Volume for the song to play (0-100%)
+        Example:
+            app = App()
+            app.play_sound(Sound.Cat_Meow_1)
+            """
+        pass
+
+    def start_sound(self, name: Sound, volume=100):
+        """
+        Starts playing a sound from your device (i.e., tablet or computer).
+        The program will not wait for the sound to finish playing before proceeding to the next command.
+
+        Arguments:
+            name: Name of the sound to play
+            volume: Volume for the song to play (0-100%)
+        Example:
+            app = App()
+            app.start_sound(Sound.Cat_Meow_1)
+            """
+        pass
+
+
+class Button:
+    f"""
+    Defines a button on the PrimeHub
+    """
+
+    def wait_until_pressed(self):
+        """
+        Waits until the button is pressed.
+
+        Example:
+            hub = PrimeHub()
+            # Beep every time the Left Button is pressed
+            while True:
+            hub.left_button.wait_until_pressed()
+            hub.speaker.start_beep()
+            hub.left_button.wait_until_released()
+            hub.speaker.stop()
+        """
+        pass
+
+    def wait_until_released(self):
+        """
+        Waits until the button is released.
+
+        Example:
+            hub = PrimeHub()
+            ``# Beep every time the button is pressed
+            while True:
+            hub.left_button.wait_until_pressed()
+            hub.speaker.start_beep()
+            hub.left_button.wait_until_released()
+            hub.speaker.stop()``
+        """
+        pass
+
+    def was_pressed(self) -> bool:
+        """
+        Tests to see whether the button has been pressed since the last time this method called.
+        Once this method returns "true," the button must be released and pressed again before it will return "true" again.
+
+        Example:
+             hub = PrimeHub()
+             while True:
+             wait_for_seconds(5)
+             if hub.left_button.was_pressed():
+             # Do something
+        """
+        pass
+
+    def is_pressed(self) -> bool:
+        """Tests whether the button is pressed.
+        Returns:
+            Weather the button is pressed
+        """
+        pass
+
+
+class Component:
+    def __init__(self, port: Port):
+        """Defines a Component, this is an Object that has a port to it such as a Motor or a Color Sensor
+
+        Parameters:
+            port: Port of the component
+        """
+        self.port = port
+
+
+class ColorSensor(Component):
+    """
+
+
+    """
+    def get_color(self) -> Color:
+        pass
+
+    def get_ambient_light(self) -> int:
+        pass
+
+    def get_reflected_light(self) -> int:
+        pass
+
+    def get_rgb_intensity(self) -> tuple:
+        pass
+
+    def get_red(self) -> int:
+        pass
+
+    def get_green(self) -> int:
+        pass
+
+    def get_blue(self) -> int:
+        pass
+
+    def wait_until_color(self, color: Color):
+        pass
+
+    def wait_for_new_color(self) -> Color:
+        pass
+
+    def light_up_all(self, brightness=100) -> int:
+        pass
+
+    def light_up(self, light_1: int = 100, light_2: int = 100, light_3: int = 100):
+        pass
+
+
+class DistanceSensor(Component):
+    def light_up_all(self, brightness: int = 100):
+        pass
+
+    def light_up(self, right_top: int = 100, left_top: int = 100, right_bottom: int = 100, left_bottom: int = 100):
+        pass
+
+    def get_distance_cm(self, short_range: bool = False) -> float:
+        pass
+
+    def get_distance_inches(self, short_range: bool = False) -> float:
+        pass
+
+    def get_distance_percentage(self, short_range: bool = False) -> int:
+        pass
+
+    def wait_for_distance_farther_than(self, distance: float, unit: AdvancedDistanceUnit = AdvancedDistanceUnit.CM,
+                                       short_range: bool = False):
+        pass
+
+    def wait_for_distance_closer_than(self, distance: float, unit: AdvancedDistanceUnit = AdvancedDistanceUnit.CM,
+                                      short_range: bool = False):
+        pass
+
+
+class ForceSensor(Component):
+    def is_pressed(self) -> bool:
+        pass
+
+    def get_force_newton(self) -> float:
+        pass
+
+    def get_force_percentage(self) -> int:
+        pass
+
+    def wait_until_pressed(self):
+        pass
+
+    def wait_until_released(self):
+        pass
+
+
+class LightMatrix:
+    def show_image(self, image: Image, brightness: int = 100):
+        pass
+
+    def set_pixel(self, x: int, y: int, brightness: int = 100):
+        pass
+
+    def write(self, text: str):
+        pass
+
+    def off(self):
+        pass
+
+
+class MotionSensor:
+    def was_gesture(self, gesture: Gesture) -> bool:
+        pass
+
+    def wait_for_new_gesture(self) -> Gesture:
+        pass
+
+    def wait_for_new_orientation(self) -> Orientation:
+        pass
+
+    def get_orientation(self) -> Orientation:
+        pass
+
+    def get_gesture(self) -> Gesture:
+        pass
+
+    def get_roll_angle(self) -> int:
+        pass
+
+    def get_pitch_angle(self) -> int:
+        pass
+
+    def get_yaw_angle(self) -> int:
+        pass
+
+    def reset_yaw_angle(self):
+        pass
+
+
+# TODO: FIX THIS CLASS!
+class Motor(Component):
+
+    def start(self, steering: int = 0, speed: int = None):
+        pass
+
+    def stop(self):
+        pass
+
+    def move_tank(self, amount: float, unit: MotorUnit = MotorUnit.CM, left_speed: int = None, right_speed: int = None):
+        pass
+
+    def start_tank(self, left_speed: int, right_speed: int):
+        pass
+
+    def start_at_power(self, power: int, steering: int = 0):
+        pass
+
+    def start_tank_at_power(self, left_power: int, right_power: int):
+        pass
+
+    def get_default_speed(self) -> int:
+        pass
+
+    def set_motor_rotation(self, amount: float = 17.6, unit: MotorUnit = MotorUnit.CM):
+        pass
+
+    def set_default_speed(self, speed: int = 100):
+        pass
+
+    def set_stop_action(self, action: StopAction = StopAction.COAST):
+        pass
+
+
+class MotorPair:
+    def __init__(self, motor_1: Port, motor_2: Port):
+        pass
+
+    def move(self, amount: float, unit: MotorUnit = MotorUnit.CM, steering: int = 0, speed: int = None):
+        pass
+
+    def start(self, steering: int = 0, speed: int = None):
+        pass
+
+    def stop(self):
+        pass
+
+    def move_tank(self, amount: float, unit: MotorUnit = MotorUnit.CM, left_speed: int = None, right_speed: int = None):
+        pass
+
+    def start_tank(self, left_speed: int, right_speed: int):
+        pass
+
+    def start_at_power(self, power: int, steering: int = 0):
+        pass
+
+    def get_default_speed(self) -> int:
+        pass
+
+    def set_motor_rotation(self, amount: float = 17.6, unit: DistanceUnit = DistanceUnit.CM):
+        pass
+
+    def set_default_speed(self, speed: int = 100):
+        pass
+
+    def set_stop_action(self, action: StopAction = StopAction.COAST):
+        pass
+
+
+class Speaker:
+    def beep(self, note: float = 60, seconds: float = 0.2):
+        pass
+
+    def start_beep(self, note: float = 60):
+        pass
+
+    def stop(self):
+        pass
+
+    def get_volume(self) -> int:
+        pass
+
+    def set_volume(self, volume: int):
+        pass
+
+
+class StatusLight:
+    def on(self, color: Color = Color.WHITE):
+        pass
+
+    def off(self):
+        pass
+
+
+class Timer:
+    def reset(self):
+        pass
+
+    def now(self) -> int:
+        pass
+
+
+class PrimeHub:
+    left_button = Button()
+    right_button = Button()
+    light_matrix = LightMatrix()
+    motion_sensor = MotionSensor()
+    speaker = Speaker()
+    status_light = StatusLight()
